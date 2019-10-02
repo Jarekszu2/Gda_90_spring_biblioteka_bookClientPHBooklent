b1e2707564860b511645d860d40ac853aa3d1258
Paweł Recław <pawel.reclaw@gmail.com>
2019-10-02 20:58:16 +0200

    Done publishing house

--- src/main/java/com/javagda25/biblioteca/controller/PublishingHouseController.java
+++ src/main/java/com/javagda25/biblioteca/controller/PublishingHouseController.java
@@ -7,12 +7,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/ph/")
@@ -32,6 +34,24 @@ public class PublishingHouseController {
     public String add(Model model, PublishingHouse publishingHouse) {
         model.addAttribute("publishingHouse", publishingHouse);
         return "ph-add";
+    }
+
+    @GetMapping("/remove/{id}")
+    public String add(@PathVariable(name = "id") Long id) {
+        publishingHouseService.remove(id);
+        return "redirect:/ph/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String add(Model model,
+                      @PathVariable(name = "id") Long id) {
+        Optional<PublishingHouse> publishingHouseOptional = publishingHouseService.getById(id);
+        if (publishingHouseOptional.isPresent()) {
+            model.addAttribute("publishingHouse", publishingHouseOptional.get());
+
+            return "ph-add";
+        }
+        return "redirect:/ph/list";
     }
 
     @PostMapping("/add")
--- src/main/java/com/javagda25/biblioteca/service/PublishingHouseService.java
+++ src/main/java/com/javagda25/biblioteca/service/PublishingHouseService.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -19,5 +20,13 @@ public class PublishingHouseService {
 
     public void save(PublishingHouse publishingHouse) {
         publishingHouseRepository.save(publishingHouse);
+    }
+
+    public Optional<PublishingHouse> getById(Long id) {
+        return publishingHouseRepository.findById(id);
+    }
+
+    public void remove(Long id) {
+        publishingHouseRepository.deleteById(id);
     }
 }
